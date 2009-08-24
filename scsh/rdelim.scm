004ca682966cb282f9e6d8ab3ee5b1a9db7087b3
Roderic Morris <roderic@ccs.neu.edu>
2009-08-24 19:29:02 -0400

    Get rid of whitespace and tabs in rdelim.

--- scsh/rdelim.scm
+++ scsh/rdelim.scm
@@ -29,45 +29,45 @@
 
 (define (read-delimited delims . args)
   (let-optionals args ((port         (current-input-port))
-		       (delim-action 'trim))
-	(let ((substr (lambda (s end)		; Smart substring.
-			(if (= end (string-length s)) s
-			    (substring s 0 end))))
-	      (delims (x->char-set delims))
-	      (gobble? (not (eq? delim-action 'peek))))
+                       (delim-action 'trim))
+        (let ((substr (lambda (s end)           ; Smart substring.
+                        (if (= end (string-length s)) s
+                            (substring s 0 end))))
+              (delims (x->char-set delims))
+              (gobble? (not (eq? delim-action 'peek))))
       ;; BUFLEN is total amount of buffer space allocated to date.
       (let lp ((strs '()) (buflen 80) (buf (make-string 80)))
-	(receive (terminator num-read)
-	         (%read-delimited! delims buf gobble? port)
-	  (if terminator
+        (receive (terminator num-read)
+                 (%read-delimited! delims buf gobble? port)
+          (if terminator
 
-	      ;; We are done. NUM-READ is either a read count or EOF.
-	      (let ((retval (if (and (zero? num-read)
-				     (eof-object? terminator)
-				     (null? strs))
-				terminator		; EOF -- got nothing.
+              ;; We are done. NUM-READ is either a read count or EOF.
+              (let ((retval (if (and (zero? num-read)
+                                     (eof-object? terminator)
+                                     (null? strs))
+                                terminator              ; EOF -- got nothing.
 
-				;; Got something. Stick all the strings
-				;; together, plus the terminator if the
-				;; client said 'CONCAT.
-				(let ((s (substr buf num-read)))
-				  (cond ((and (eq? delim-action 'concat)
-					      (char? terminator))
-					 (apply string-append
-						(reverse `(,(string terminator)
-							   ,s . ,strs))))
+                                ;; Got something. Stick all the strings
+                                ;; together, plus the terminator if the
+                                ;; client said 'CONCAT.
+                                (let ((s (substr buf num-read)))
+                                  (cond ((and (eq? delim-action 'concat)
+                                              (char? terminator))
+                                         (apply string-append
+                                                (reverse `(,(string terminator)
+                                                           ,s . ,strs))))
 
-					((null? strs) s)    ; Gratuitous opt.
-					(else (apply string-append
-						     (reverse (cons s strs)))))))))
-		(if (eq? delim-action 'split)
-		    (values retval terminator)
-		    retval))
+                                        ((null? strs) s)    ; Gratuitous opt.
+                                        (else (apply string-append
+                                                     (reverse (cons s strs)))))))))
+                (if (eq? delim-action 'split)
+                    (values retval terminator)
+                    retval))
 
-	      ;; We are not done. Loop and read in some more.
-	      (lp (cons buf strs)
-		  (+ buflen buflen)
-		  (make-string buflen))))))))
+              ;; We are not done. Loop and read in some more.
+              (lp (cons buf strs)
+                  (+ buflen buflen)
+                  (make-string buflen))))))))
 
 
 ;;; (read-delimited! delims buf [port delim-action start end])
@@ -80,7 +80,7 @@
 ;;; DELIM-ACTION determines what to do with the terminating delimiter;
 ;;; it is as in READ-DELIMITED.
 ;;;
-;;; In determining the return value, there is an ambiguous case: when the 
+;;; In determining the return value, there is an ambiguous case: when the
 ;;; buffer is full, *and* the following char is a delimiter char or EOF.
 ;;; Ties are broken favoring termination over #f -- after filling the buffer,
 ;;; READ-DELIMITED! won't return #f until it has peeked one past the end
@@ -93,53 +93,53 @@
 
 (define (read-delimited! delims buf . args) ; [port delim-action start end]
   (let-optionals args ((port         (current-input-port))
-		       (delim-action 'trim)
-		       (start        0)
-		       (end          (string-length buf)))
+                       (delim-action 'trim)
+                       (start        0)
+                       (end          (string-length buf)))
     (receive (terminator num-read)
-	     (%read-delimited! delims buf
-			       (not (eq? delim-action 'peek)) ;Gobble delim?
-			       port
-			       start
-			       (if (eq? delim-action 'concat)
-				   (- end 1) ; Room for terminator.
-				   end))
+             (%read-delimited! delims buf
+                               (not (eq? delim-action 'peek)) ;Gobble delim?
+                               port
+                               start
+                               (if (eq? delim-action 'concat)
+                                   (- end 1) ; Room for terminator.
+                                   end))
 
-      (if terminator	; Check for buffer overflow.
-	  (let ((retval (if (and (zero? num-read)
-				 (eof-object? terminator))
-			    terminator	; EOF -- got nothing.
-			    num-read))) ; Got something.
+      (if terminator    ; Check for buffer overflow.
+          (let ((retval (if (and (zero? num-read)
+                                 (eof-object? terminator))
+                            terminator  ; EOF -- got nothing.
+                            num-read))) ; Got something.
 
-	    (case delim-action
-	      ((peek trim)	retval)
-	      ((split)	(values retval terminator))
-	      ((concat)	(cond ((char? terminator)
-			       (string-set! buf (+ start num-read) terminator)
-			       (+ num-read 1))
-			      (else retval)))))
+            (case delim-action
+              ((peek trim)      retval)
+              ((split)  (values retval terminator))
+              ((concat) (cond ((char? terminator)
+                               (string-set! buf (+ start num-read) terminator)
+                               (+ num-read 1))
+                              (else retval)))))
 
-	  ;; Buffer overflow.
-	  (case delim-action
-	    ((peek trim) #f)
-	    ((split)     (values #f #f))
-	    ((concat)    (let ((last (read-char port)))
-			   (if (char? last)
-			       (string-set! buf (+ start num-read) last))
-			   (and (or (eof-object? last)
-				    (char-set-contains? (x->char-set delims)
-							last))
-				(+ num-read 1)))))))))
-		  
+          ;; Buffer overflow.
+          (case delim-action
+            ((peek trim) #f)
+            ((split)     (values #f #f))
+            ((concat)    (let ((last (read-char port)))
+                           (if (char? last)
+                               (string-set! buf (+ start num-read) last))
+                           (and (or (eof-object? last)
+                                    (char-set-contains? (x->char-set delims)
+                                                        last))
+                                (+ num-read 1)))))))))
+
 
 ;;; (%read-delimited! delims buf gobble? [port start end])
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; This low-level routine uses a different interface. It returns two values:
 ;;; - TERMINATOR: A value describing why the read was terminated:
-;;;   + character or eof-object => read terminated by this value; 
+;;;   + character or eof-object => read terminated by this value;
 ;;;   + #f                      => filled buffer w/o terminating read.
 ;;; - NUM-READ: Number of chars read into buf.
-;;; 
+;;;
 ;;; Note:
 ;;; - Invariant: TERMINATOR = #f  =>  NUM-READ = END - START.
 ;;; - Invariant: TERMINATOR = eof-object and NUM-READ = 0 => at EOF.
@@ -157,51 +157,51 @@
 (define (port-buffer-read-delimited delims buf gobble? port start end)
   (obtain-port-lock port)
   (let ((the-port-limit (port-limit port)))
-    (let lp ((i start) (lp-port-index (port-index port))) 
+    (let lp ((i start) (lp-port-index (port-index port)))
       (cond ((port-pending-eof? port)
-	     (set-port-index! port lp-port-index)
-	     (release-port-lock port)
-	     (values (eof-object) (- i start)))
-	    ((>= i end)
-	     (set-port-index! port lp-port-index)
-	     (release-port-lock port)
-	     (values #f (- i start)))
-	    ((< lp-port-index the-port-limit)
-	     (let ((the-read-char 
-		    (ascii->char (byte-vector-ref 
-				  (port-buffer port) lp-port-index))))
-	       (if (char-set-contains? delims the-read-char)
-		   (begin 
-		     (if gobble? 
-			 (set-port-index! port (+ lp-port-index 1))
-			 (set-port-index! port lp-port-index))
-		     (release-port-lock port)
-		     (values the-read-char (- i start)))
-		   (begin
-		     (string-set! buf i the-read-char)
-		     (lp (+ i 1) (+ lp-port-index 1))))))
-	    (else  (set-port-index! port 0)
-		   (set-port-limit! port 0)
-		   (release-port-lock port)
-		   (values 'port-buffer-exhausted (- i start)))))))
-		 
+             (set-port-index! port lp-port-index)
+             (release-port-lock port)
+             (values (eof-object) (- i start)))
+            ((>= i end)
+             (set-port-index! port lp-port-index)
+             (release-port-lock port)
+             (values #f (- i start)))
+            ((< lp-port-index the-port-limit)
+             (let ((the-read-char
+                    (ascii->char (byte-vector-ref
+                                  (port-buffer port) lp-port-index))))
+               (if (char-set-contains? delims the-read-char)
+                   (begin
+                     (if gobble?
+                         (set-port-index! port (+ lp-port-index 1))
+                         (set-port-index! port lp-port-index))
+                     (release-port-lock port)
+                     (values the-read-char (- i start)))
+                   (begin
+                     (string-set! buf i the-read-char)
+                     (lp (+ i 1) (+ lp-port-index 1))))))
+            (else  (set-port-index! port 0)
+                   (set-port-limit! port 0)
+                   (release-port-lock port)
+                   (values 'port-buffer-exhausted (- i start)))))))
+
 
 
 (define (%read-delimited! delims buf gobble? . args)
   (let-optionals args ((port  (current-input-port))
-		       (start 0)
-		       (end   (string-length buf)))
+                       (start 0)
+                       (end   (string-length buf)))
     (if (immutable? buf)
         (error "Immutable buffer argument to %read-delimited!" buf))
     (let ((delims (x->char-set delims)))
       (let lp ((start start) (total 0))
-	(receive (terminator num-read) 
-	    (port-buffer-read-delimited delims buf gobble? port start end)
-	  (if (not (eq? terminator 'port-buffer-exhausted))
-	      (values terminator (+ num-read total))
-	      (begin (peek-char port)	; kludge to fill the buffer
-		     (lp (+ start num-read) (+ total num-read)))))))))
-			  
+        (receive (terminator num-read)
+            (port-buffer-read-delimited delims buf gobble? port start end)
+          (if (not (eq? terminator 'port-buffer-exhausted))
+              (values terminator (+ num-read total))
+              (begin (peek-char port)   ; kludge to fill the buffer
+                     (lp (+ start num-read) (+ total num-read)))))))))
+
 
 
 ; overwrites port-index :-(
@@ -212,33 +212,33 @@
 
 (define (skip-char-set skip-chars . maybe-port)
   (let ((port (:optional maybe-port (current-input-port)))
-	(cset (x->char-set skip-chars)))
-    
+        (cset (x->char-set skip-chars)))
+
     (let lp ((total 0))
       (receive (succ num-read) (buffer-skip-char-set cset port)
-	(if (not succ)
-	    (+ total num-read)		; eof
-	    (begin (peek-char port)	; kludge to fill the buffer
-		   (lp  (+ total num-read))))))))
+        (if (not succ)
+            (+ total num-read)          ; eof
+            (begin (peek-char port)     ; kludge to fill the buffer
+                   (lp  (+ total num-read))))))))
 
 (define (buffer-skip-char-set cset port)
   (let ((the-port-limit (port-limit port)))
     (let lp ((lp-port-index (port-index port)) (i 0))
       (cond ((port-pending-eof? port)
-	     (set-port-index! port lp-port-index)
-	     (values #f i))
-	    ((< lp-port-index the-port-limit)
-	     (let ((the-read-char 
-		    (ascii->char (byte-vector-ref 
-				  (port-buffer port) lp-port-index))))
-	       (cond ((char-set-contains? cset the-read-char)
-		      (lp (+ lp-port-index 1) (+ i 1)))
-		     (else 
-		      (set-port-index! port lp-port-index)
-		      (values #f i)))))
-	    (else (set-port-index! port 0)
-		  (set-port-limit! port 0)
-		  (values 'port-buffer-exhausted i))))))
+             (set-port-index! port lp-port-index)
+             (values #f i))
+            ((< lp-port-index the-port-limit)
+             (let ((the-read-char
+                    (ascii->char (byte-vector-ref
+                                  (port-buffer port) lp-port-index))))
+               (cond ((char-set-contains? cset the-read-char)
+                      (lp (+ lp-port-index 1) (+ i 1)))
+                     (else
+                      (set-port-index! port lp-port-index)
+                      (values #f i)))))
+            (else (set-port-index! port 0)
+                  (set-port-limit! port 0)
+                  (values 'port-buffer-exhausted i))))))
 
 ;;; (read-line [port delim-action])
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -257,33 +257,33 @@
 
 (define (read-paragraph . args)
   (let-optionals args ((port         (current-input-port))
-		       (handle-delim 'trim))
+                       (handle-delim 'trim))
     ;; First, skip all blank lines.
     (let lp ()
       (let ((line (read-line port 'concat)))
-	(cond ((eof-object? line)
-	       (if (eq? handle-delim 'split) (values line line) line))
+        (cond ((eof-object? line)
+               (if (eq? handle-delim 'split) (values line line) line))
 
-	      ((regexp-search? blank-line-regexp line) (lp))
+              ((regexp-search? blank-line-regexp line) (lp))
 
-	      ;; Then, read in non-blank lines.
-	      (else
-	       (let lp ((lines (list line)))
-		 (let ((line (read-line port 'concat)))
-		   (if (and (string? line)
-			    (not (regexp-search? blank-line-regexp line)))
+              ;; Then, read in non-blank lines.
+              (else
+               (let lp ((lines (list line)))
+                 (let ((line (read-line port 'concat)))
+                   (if (and (string? line)
+                            (not (regexp-search? blank-line-regexp line)))
 
-		       (lp (cons line lines))
+                       (lp (cons line lines))
 
-		       ;; Return the paragraph
-		       (let ((->str (lambda (lns) (apply string-append (reverse lns)))))
-			 (case handle-delim
-			   ((trim) (->str lines))
+                       ;; Return the paragraph
+                       (let ((->str (lambda (lns) (apply string-append (reverse lns)))))
+                         (case handle-delim
+                           ((trim) (->str lines))
 
-			   ((concat)
-			    (->str (if (eof-object? line) lines (cons line lines))))
+                           ((concat)
+                            (->str (if (eof-object? line) lines (cons line lines))))
 
-			   ((split)
-			    (values (->str lines) line))
+                           ((split)
+                            (values (->str lines) line))
 
-			   (else (error "Illegal HANDLE-DELIM parameter to READ-PARAGRAPH")))))))))))))
+                           (else (error "Illegal HANDLE-DELIM parameter to READ-PARAGRAPH")))))))))))))
