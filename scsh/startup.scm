b25f559ce9f97ae9afe70269dc03108e05b2b558
Roderic Morris <roderic@ccs.neu.edu>
2009-08-24 21:03:20 -0400

    Remove whitespace and tabs in startup.scm.

--- scsh/startup.scm
+++ scsh/startup.scm
@@ -1,7 +1,7 @@
 ;;; Scsh start-up code.
 ;;; Copyright (c) 1995 by Olin Shivers.
 
-;;; A scsh starter takes the command line args, parses them, 
+;;; A scsh starter takes the command line args, parses them,
 ;;; initialises the scsh system, and either starts up a repl loop
 ;;; or executes the -s script.
 
@@ -15,7 +15,7 @@
 ;;; initialise ERROR-OUTPUT-PORT to stderr -- this is a bug in the vm's
 ;;; handoff to the very first Scheme form (it passes two ports -- not three).
 ;;; Until Kelsey fixes these, we hack it with these replacements, which
-;;; invoke INIT-SCSH-HINDBRAIN, which re-initialises the I/O system to be 
+;;; invoke INIT-SCSH-HINDBRAIN, which re-initialises the I/O system to be
 ;;; what you wanted.
 
 ;;; WRITE-IMAGE calls the starter after installing a fatal top-level
@@ -26,10 +26,10 @@
     (display (string-append "Writing " filename) (command-output))
     (newline (command-output))
     ;JMG: it is set to #f in the vm, so I omit it now
-;;;(flush-the-symbol-table!)	;Gets restored at next use of string->symbol
+;;;(flush-the-symbol-table!)    ;Gets restored at next use of string->symbol
     (write-image filename
-		 (scsh-stand-alone-resumer start)
-		 "Scsh 0.6")
+                 (scsh-stand-alone-resumer start)
+                 "Scsh 0.6")
     #t))
 
 
@@ -45,47 +45,47 @@
 (define (dump-scsh-program start filename)
   (let ((context (user-context)))
     (really-dump-scsh-program (lambda (args)
-				(with-scsh-initialized
-				 #f context args 
-				 (lambda ()
-				   (start args))))
-			      filename)))
+                                (with-scsh-initialized
+                                 #f context args
+                                 (lambda ()
+                                   (start args))))
+                              filename)))
 
 (define (scsh-stand-alone-resumer start)
-  (usual-resumer			; sets up exceptions, interrupts,
-					; and current input & output
-   (lambda (args)			; VM gives us our args, but not our program.
+  (usual-resumer                        ; sets up exceptions, interrupts,
+                                        ; and current input & output
+   (lambda (args)                       ; VM gives us our args, but not our program.
      (init-fdports!)
      (call-with-current-continuation
       (lambda (halt)
         (set! %vm-prog-args args)
-	(set-command-line-args! %vm-prog-args)
-	(with-handler 
-	 (simple-condition-handler halt (current-error-port))
-	 (lambda ()
-	   (let ((dynamic-env (get-dynamic-env))
-		 (*result* 4711))
-	     (let ((runnable (make-queue))
-		   (thread (make-thread (lambda ()
-					  (set! *result*
-						(start (command-line))))
-					dynamic-env
-					'scsh-initial-thread))
-		   (thread-count (make-counter)))
-	      
-	       (enqueue! runnable thread)
-	       (increment-counter! thread-count)
-	      
-	       (run-threads
-		(round-robin-event-handler runnable 200 dynamic-env thread-count
-					   (lambda args #f)
-					   (lambda (thread token args) ; upcall handler
-					     (propogate-upcall thread token args))
-					   (lambda ()
-					     (if (positive? (counter-value thread-count))
-						 ((structure-ref threads-internal wait))
-						 #f))))
-	       (if (integer? *result*) *result* 0)))))))))) ; work around bug.
+        (set-command-line-args! %vm-prog-args)
+        (with-handler
+         (simple-condition-handler halt (current-error-port))
+         (lambda ()
+           (let ((dynamic-env (get-dynamic-env))
+                 (*result* 4711))
+             (let ((runnable (make-queue))
+                   (thread (make-thread (lambda ()
+                                          (set! *result*
+                                                (start (command-line))))
+                                        dynamic-env
+                                        'scsh-initial-thread))
+                   (thread-count (make-counter)))
+
+               (enqueue! runnable thread)
+               (increment-counter! thread-count)
+
+               (run-threads
+                (round-robin-event-handler runnable 200 dynamic-env thread-count
+                                           (lambda args #f)
+                                           (lambda (thread token args) ; upcall handler
+                                             (propogate-upcall thread token args))
+                                           (lambda ()
+                                             (if (positive? (counter-value thread-count))
+                                                 ((structure-ref threads-internal wait))
+                                                 #f))))
+               (if (integer? *result*) *result* 0)))))))))) ; work around bug.
 
 (define %vm-prog-args #f)
- 
+
