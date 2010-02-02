c5bfb176f669b775809ee1313baee7568ef92556
Roderic Morris <roderyc@gmail.com>
2010-02-01 23:22:55 -0800

    Update the verison number.

--- scsh/scsh-version.scm
+++ scsh/scsh-version.scm
@@ -1,5 +1,5 @@
 (define scsh-major-version 0)
-(define scsh-minor-version 6)
+(define scsh-minor-version 7)
 
-(define scsh-version-string "0.6.7")
-(define scsh-release-name "R6RS")
+(define scsh-version-string "0.7")
+(define scsh-release-name "Revival")
